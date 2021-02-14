b4c161d7a1c2c05d4421028f7bea4881a1e93cd3
Tomáš Polívka <tomas@pop-os.localdomain>
2021-02-14 15:57:01 +0100

    prepare for deployment

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -2,15 +2,31 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./config/db.js"
 import productRoutes from "./routes/productRoutes.js"
+import path from "path"
 
 const app = express()
 dotenv.config()
 connectDb()
+const __dirname = path.resolve()
 
 app.listen(process.env.SERVER_PORT)
 
-app.get("/", (req, res) => {
-	res.send("api is running")
-})
-
 app.use("/api/products", productRoutes)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(
+		express.static(
+			path.join(__dirname, "/ecommerce-frontend/build")
+		)
+	)
+	app.get("*", (req, res) =>
+		res.sendFile(
+			path.resolve(
+				__dirname,
+				"ecommerce-frontend",
+				"build",
+				"index.html"
+			)
+		)
+	)
+}
